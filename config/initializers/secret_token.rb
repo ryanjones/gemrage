208a3f6783c85a388fb7ea2e545e1c4c7064819c
Fletcher Nichol <fnichol@nichol.ca>
2010-10-15 18:24:12 -0600

    Initial project skeleton. Rage!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gemrage::Application.config.secret_token = 'c6db9364569e7cd24ffdbc5145cb80dce44633851b8a342a0ac5df01b7a3172b16bf24f73705d1aa079940135ba7866f06d5797d4497020aa28232779f89de96'
