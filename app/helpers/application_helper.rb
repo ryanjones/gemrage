237beb5e957cf8c6df2022d01f29dbf0b0eea5c0
Ryan <ryan.michael.jones@gmail.com>
2010-10-17 14:19:29 -0600

    google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,22 @@ module ApplicationHelper
   def auth_path(provider)
     "/auth/#{provider.to_s}"
   end
+  
+  def google_analytics
+    <<-EOF
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-18119673-2']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
+    EOF
+  end
 end
