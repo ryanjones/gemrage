48e7b5fb2c3a80c3845771c398912fcf2cadc1ce
Nathan Youngman <git@nathany.com>
2010-10-16 18:31:56 -0600

    memoized equivalent to Platform.find_by_code()

--- spec/models/platform_spec.rb
+++ spec/models/platform_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe Platform do
+  before(:each) do
+    @mri = Platform.create(:code => 'mri', :name => 'Matz Ruby')
+    @ree = Platform.create(:code => 'ree', :name => 'Enterpisey')
+  end
+  
+  it do 
+    @mri.should be_valid 
+    @ree.should be_valid
+  end
+  
+  it "should return id for a code" do
+    Platform.id_for_code('mri').should == @mri.id
+    Platform.id_for_code('ree').should == @ree.id
+  end
+  
+  
 end
