8f542385986da80593cc87d6f2c85dfa03a4290d
Nathan Youngman <git@nathany.com>
2010-10-16 18:32:55 -0600

    platform/version hash to an array suitable for insert

--- app/models/installed_gem.rb
+++ app/models/installed_gem.rb
@@ -1,6 +1,25 @@
 class InstalledGem < ActiveRecord::Base
   belongs_to :user
-  belongs_to :rubygem
   belongs_to :machine
+  
+  belongs_to :rubygem
   belongs_to :platform
+  
+  # { name => { platform => version_list, ... }, ... }
+  def self.create_from_hash(payload)
+    
+  end
+
+private
+  # { platform => version_list, ... }
+  def self.attributes_from_gem_hash(data)
+    data.map do |platform, versions|
+      { :platform_id => Platform.id_for_code(platform), :version_list => versions }
+    end
+  end
+  
+  def self.purge
+
+  end
+  
 end
