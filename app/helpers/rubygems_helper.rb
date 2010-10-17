6cc1c75599023a3e75d398a181ef6b0dab0a3a62
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-10-17 14:59:00 -0600

    expire that after 30 minutes

--- app/helpers/rubygems_helper.rb
+++ app/helpers/rubygems_helper.rb
@@ -2,8 +2,8 @@ module RubygemsHelper
   def gem_stats(rg)
     {
       'Downloads' => rg.downloads,
-      'Installs' => Rails.cache.fetch([rg.cache_key, 'install-count'].join(':')) { rg.installed_gems.count },
-      'Installs (latest version)' => Rails.cache.fetch([rg.cache_key, 'latest-install-count'].join(':')) { rg.installed_gems.where("version_list LIKE ?", "%#{rg.latest_version}%").count }
+      'Installs' => Rails.cache.fetch([rg.cache_key, 'install-count'].join(':'), :expires_in => 30.minutes) { rg.installed_gems.count },
+      'Installs (latest version)' => Rails.cache.fetch([rg.cache_key, 'latest-install-count'].join(':'), :expires_in => 30.minutes) { rg.installed_gems.where("version_list LIKE ?", "%#{rg.latest_version}%").count }
     }
   end
 end
