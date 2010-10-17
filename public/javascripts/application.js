536a5b900c034b577aed3ccb3ceff7434dd336b3
Fletcher Nichol <fnichol@nichol.ca>
2010-10-17 15:32:48 -0600

    Fix the flash messages a wee bit.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".flash").fadeTo("slow", 10.00, function(){ //fade
+  $(".flash").fadeTo(5000, 0.01, function(){ //fade
     $(this).slideUp("slow", function() { //slide up
       $(this).remove(); //then remove from the DOM
     });
