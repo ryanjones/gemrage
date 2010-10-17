deb4dd229d55b28079d82584606689cd7df3a567
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-10-17 16:06:11 -0600

    project listing

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,22 +1,25 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :gems, :class_name => 'ProjectGem'
-  
+
   validates_presence_of :name
   validates_presence_of :identifier
-  
+
   # { :name => '', :identifer => '', :gems => {} }
   def self.process(payload)
     payload.each do |identifier, data|
       if data['gems'].present?
         # only set the name on create (user can edit)
-        project = find_by_identifier(identifier) || 
-                  create(:name => data['name'], 
+        project = find_by_identifier(identifier) ||
+                  create(:name => data['name'],
                          :identifier => identifier,
                          :origin => data['origin'])
         project.gems.process(project, data['gems'])
       end
     end
   end
-  
+
+  def to_param
+    "#{id}-#{name}"
+  end
 end
