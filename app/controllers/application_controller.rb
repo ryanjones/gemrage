9edc6606d7f609899916f4671ed686596d8f11cb
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-10-17 16:43:03 -0600

    project name editing and destroying

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+protected
+
+  def redirect_back_or_default(default)
+    redirect_to(request.referer || default)
+  end
 end
