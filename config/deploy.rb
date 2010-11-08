986080d5ecb94886d08ed3f335ac8df23fce344d
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-11-07 21:21:28 -0700

    point deploy.rb to the right place

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require "bundler/capistrano"
 
-server '178.79.143.150', :app, :web, :db, :primary => true
+server 'gemrage.com', :app, :web, :db, :primary => true
 
 set :bundle_flags,    "--deployment"
 set :bundle_without,  [:development, :test]
